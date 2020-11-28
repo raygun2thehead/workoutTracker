b4cf78546f5e7ecff42dfc2377ed092b58ed28a3
Steven Schaab <stevenes83@gmail.com>
2020-11-27 19:33:45 -0600

    can add exercises to workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,36 +1,41 @@
 const db = require("../models");
 
-module.exports = function(app){
-app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout)
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
-
-app.put("/api/workouts/:id", ({body, params}, res) => {
-    const id = params.id;
-    db.Workout.updateOne({_id:req.params.id}, {excercise})
-    .then(dbWorkout => {
-        res.json(dbWorkout)
-    })
-    .catch(err => {
-        res.status(400).json(err);
+module.exports = function (app) {
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
     });
-})
+
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        console.log(body);
+        const id = params.id; 
+        
+        db.Workout.findByIdAndUpdate({_id: id}, { $push: { exercises: body }})
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+                console.log(dbWorkout)
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    })
 
 
-app.post("/api/workouts", async (req, res)=> {
-    try{
-        const response = await db.Workout.create({type: "workout"})
-        res.json(response);
-    }
-    catch(err){
-        console.log("error occurred creating a workout: ", err)
-    }
-})
+    app.post("/api/workouts", async (req, res) => {
+        try {
+            await db.Workout.create({ type: "workout" })
+                .then(dbWorkout => {
+                    res.json(dbWorkout);
+                })
+        }
+        catch (err) {
+            console.log("error occurred creating a workout: ", err)
+        }
+    })
 }
 
