3fb8ad631d066c5ba65f90971ec495a448bfcc12
Steven Schaab <stevenes83@gmail.com>
2020-12-04 15:58:30 -0600

    stats page finished/project finished?

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,9 +13,9 @@ module.exports = function (app) {
 
     app.put("/api/workouts/:id", ({ body, params }, res) => {
         console.log(body);
-        const id = params.id; 
-        
-        db.Workout.findByIdAndUpdate({_id: id}, { $push: { exercises: body }})
+        const id = params.id;
+
+        db.Workout.findByIdAndUpdate({ _id: id }, { $push: { exercises: body } })
             .then(dbWorkout => {
                 res.json(dbWorkout)
                 console.log(dbWorkout)
@@ -26,16 +26,26 @@ module.exports = function (app) {
     })
 
 
-    app.post("/api/workouts", async (req, res) => {
-        try {
-            await db.Workout.create({ type: "workout" })
-                .then(dbWorkout => {
-                    res.json(dbWorkout);
-                })
-        }
-        catch (err) {
-            console.log("error occurred creating a workout: ", err)
-        }
+    app.post("/api/workouts", (req, res) => {
+
+        db.Workout.create({ type: "workout" })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+
+            .catch(err => {
+                console.log("error occurred creating a workout: ", err);
+            });
     })
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
 }
 
